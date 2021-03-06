646f6cf674ac5d5eb86b5c74be8310ef58bc16ec
sujanichathurika <chathusujani57@gmail.com>
2021-03-06 10:34:30 +0530

    practice lab sheet-3 in PAF

---
+++ LABSHEET3/src/LAB3/Main.java
@@ -0,0 +1,11 @@
+package LAB3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first project at SLIIT");
+
+	}
+
+}
